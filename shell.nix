bc074f1eb9d1defe02d58952312d1a9cfd6b6641
C. R. Oldham <cro@ncbt.org>
2021-12-02 03:55:26 +0000

    nix shell

--- shell.nix
+++ shell.nix
@@ -1,15 +1,43 @@
 # https://nix.dev/tutorials/declarative-and-reproducible-developer-environments
 with import <nixpkgs> { };
-
-mkShell {
-
-  # Package names can be found via https://search.nixos.org/packages
-  nativeBuildInputs = [
-    direnv
+let
+  # define packages to install with special handling for OSX
+  basePackages = [
+    gnumake
+    gcc
+    readline
+    openssl
+    zlib
+    libxml2
+    curl
+    libiconv
+    elixir_1_12
+    glibcLocales
+    nodejs-16_x
+    yarn
+    postgresql
   ];
 
-  NIX_ENFORCE_PURITY = true;
-
-  shellHook = ''
+  inputs = basePackages
+    ++ lib.optional stdenv.isLinux inotify-tools
+    ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+        CoreFoundation
+        CoreServices
+      ]);
+  # define shell startup command
+  hooks = ''
+    # this allows mix to work on the local directory
+    mkdir -p .nix-mix
+    mkdir -p .nix-hex
+    export MIX_HOME=$PWD/.nix-mix
+    export HEX_HOME=$PWD/.nix-hex
+    export PATH=$MIX_HOME/bin:$PATH
+    export PATH=$HEX_HOME/bin:$PATH
+    export LANG=en_US.UTF-8
+    export ERL_AFLAGS="-kernel shell_history enabled"
   '';
+
+in mkShell {
+  buildInputs = inputs;
+  shellHook = hooks;
 }
